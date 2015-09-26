0a5ec03aae5ec8741e0187c6e65c51c8c8c78b0d
shahlusine89 <shahlusine89@gmail.com>
2015-09-26 13:00:11 +0200

    Solved the Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a matrix, that cashes its inverse. After computing the inverse it
+## saves the invers, which saves the computational effort.
+## This method creates a matrix and its methods: set(changes the values of matrix), get(gets the values of matrix), 
+##set_inv (changes the values of the invers matrix), get_inv(gets the values of the invers matrix).
 
-## Write a short comment describing this function
+
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv_x <- NULL
+  set <- function(y) {
+  x <<- y
+  inv_x<<- NULL
+  }
+  
+  get <- function() x
+  set_inv <- function(inv) inv_x <<- inv
+  get_inv <- function() inv_x
+  
+  list(set=set, get=get,
+       set_inv=set_inv,
+       get_inv=get_inv)
+  }
 
-}
-
-
-## Write a short comment describing this function
+## Here we see whether the inverse is already cashed. In case it it, we return the values. In case it is not, 
+##we do the inversion and save in inv_x
 
 cacheSolve <- function(x, ...) {
-  asd
-        ## Return a matrix that is the inverse of 'x'
+  ## Return the invers of x
+  temp_inv=x$get_inv()
+  if(!is.null(temp_inv)) {
+    return(temp_inv)
+  }
+  
+  
+  x_matrix <- x$get()
+  temp_inv <- solve(x_matrix,...)
+  x$set_inv(temp_inv)
+  temp_inv
 }
